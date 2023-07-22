3e4c25665c8628dc2c876c62fd61a464695d553b
heelws <162127@naver.com>
2023-07-22 19:43:03 +0900

    UI 업데이트 구현
    user가 작성한 값을 UI에 자동으로 업데이트

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
+import { useState } from "react";
 import "./App.css";
 import PaymentForm from "./components/PaymentForm/PaymentForm";
 import Expenses from "./components/Payments/Expenses";
-
+// paymentform.js의 부모 컴포넌트인 app.js에서 console 찍었을 때 자식 컴포넌트의 state 값이 찍히도록
 function App() {
-	const expenses = [
-		{
-			id: "e1",
-			title: "수건",
-			amount: 12.33,
-			date: new Date(2025, 8, 14),
-		},
-	];
+  const [expenses, setExpenses] = useState([
+    {
+      id: "e1",
+      title: "수건",
+      amount: 12.33,
+      date: new Date(2025, 8, 14),
+    },
+  ]);
 
-	return (
-		<>
-			<PaymentForm />
-			<Expenses items={expenses} />
-		</>
-	);
+  const getPaymentFormData = (data) => {
+    console.log(data);
+    setExpenses([
+      {
+        id: Math.random().toString(),
+        title: data.name,
+        amount: data.price,
+        date: data.today,
+      },
+    ]);
+  };
+
+  return (
+    <>
+      <PaymentForm getPaymentFormData={getPaymentFormData} />
+      <Expenses items={expenses} />
+    </>
+  );
 }
 
 export default App;
