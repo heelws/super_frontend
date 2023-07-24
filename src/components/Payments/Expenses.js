0a29bfd4870df506e012e1facafb68849d78b3b6
heelws <162127@naver.com>
2023-07-24 22:41:24 +0900

    list 연도별로 filter 기능 구현
    - filter 사용해 선택한 연도별로 list 나타나도록 구

--- src/components/Payments/Expenses.js
+++ src/components/Payments/Expenses.js
@@ -1,32 +1,50 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
-import ExpensesFilter from './ExpensesFilter';
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
-	const [filteredYear, setFilteredYear] = useState('2023');
+  const [filteredYear, setFilteredYear] = useState("2023");
 
-	const filterChangeHandler = (selectedYear) => {
-		setFilteredYear(selectedYear);
-	};
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
 
-	return (
-		<Card className="expenses">
-			<ExpensesFilter
-				selected={filteredYear}
-				onChangeFilter={filterChangeHandler}
-			/>
-			{props.items.map((item) => (
-				<ExpenseItem
-					title={item.title}
-					amount={item.amount}
-					date={item.date}
-				/>
-			))}
-		</Card>
-	);
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear; //4자리의 연도, number
+  });
+
+  // let expensesContent = <p>값이 없습니다</p>;
+  // if (filteredExpenses.length > 0) {
+  // expensesContent = filteredExpenses.map((item) => (
+  // <ExpenseItem title={item.title} amount={item.amount} date={item.date} />
+  // ));
+  // }
+
+  return (
+    <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+
+      {filteredExpenses.length > 0 ? (
+        filteredExpenses.map((item) => (
+          <ExpenseItem
+            title={item.title}
+            amount={item.amount}
+            date={item.date}
+          />
+        ))
+      ) : (
+        <p>값이 없습니다</p>
+      )}
+
+      {/* 조건이 true면 : 이전을 return하고 false면 : 이후 return */}
+    </Card>
+  );
 };
 
 export default Expenses;
