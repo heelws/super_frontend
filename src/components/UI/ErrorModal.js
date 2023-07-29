92de0d97cec331dc9f8495fb62ee3af5a2090d2c
heelws <162127@naver.com>
2023-07-30 02:04:56 +0900

    modal 수정
    -createPortal 사용해서 modal이 전체 HTML에서 동작하게 수정

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,24 +1,48 @@
-import React from 'react';
+//errormodal.js
+import React from "react";
+import ReactDOM from "react-dom";
 
-import Card from './Card';
-import Button from './Button';
-import classes from './ErrorModal.module.css';
+import Card from "./Card";
+import Button from "./Button";
+import classes from "./ErrorModal.module.css";
 
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onConfirm} />;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <Card className={classes.modal}>
+      <header className={classes.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={classes.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={classes.actions}>
+        <Button onClick={props.onConfirm}>Okay</Button>
+      </footer>
+    </Card>
+  );
+};
+
+//card는 모달 안의 컨텐츠 요소, backdrop은 뒷단 배경
 const ErrorModal = (props) => {
   return (
     <div>
-      <div className={classes.backdrop} onClick={props.onConfirm} />
-      <Card className={classes.modal}>
-        <header className={classes.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={classes.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={classes.actions}>
-          <Button onClick={props.onConfirm}>Okay</Button>
-        </footer>
-      </Card>
+      {ReactDOM.createPortal(
+        <Backdrop onConfirm={props.onConfirm} />,
+        document.getElementById("backdrop-root")
+        // createPortal(children(component),domnode(어디로 옮겨줄지))
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          onConfirm={props.onConfirm}
+        />,
+        document.getElementById("overlay-root")
+      )}
     </div>
   );
 };
