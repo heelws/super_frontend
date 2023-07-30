49eb4f155aa988bdd6153f14e9daa96140f7c6ff
heelws <162127@naver.com>
2023-07-30 16:13:05 +0900

    로그인 후 새로고침해도 로그인 상태 유지
    - useEffect 사용
    - localStorage에 로그인 값 저장

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  // state에서 쓰고있는 값들은 브라우저가 렌더링이 될 때마다 초기화
+  // => isLoggedIn =  true라고 해도 브라우저를 새로고침하면 다시 로그인인
+
+  useEffect(() => {
+    // 만약 로그인이 되어 있다면 isLoggedIn =  true
+    const stroedUserLoggedInInfo = localStorage.getItem("isLoggedIn");
+    console.log(stroedUserLoggedInInfo);
+    if (stroedUserLoggedInInfo === "1") setIsLoggedIn(true);
+    // 만약 로그인이 되어 있지 않다면 isLoggedIn = false
+    else setIsLoggedIn(false);
+  }, []);
 
   const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
+    //브라우저에서 로그인을 했는지 안했는지 기억할 수 있고 판단할 수 있는 값을 어딘가에 저장 => 로컬스토리지
+    localStorage.setItem("isLoggedIn", "1"); // setItem : 값을 저장할 수 있게 만들어줌('key','value')
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    // isLoggedIn을 없애주는 로직 추가
+    localStorage.removeItem("isLoggedIn");
     setIsLoggedIn(false);
   };
 
