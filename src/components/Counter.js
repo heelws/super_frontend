87dd24bad08eae41750e79b8ba73af524f61a101
heelws <162127@naver.com>
2023-08-07 03:13:42 +0900

    react-redux counter
    counter +1, -1 구현

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,13 +1,27 @@
-import classes from './Counter.module.css';
+//counter.js
+import { useDispatch, useSelector } from "react-redux";
+import classes from "./Counter.module.css";
 
 const Counter = () => {
   const toggleCounterHandler = () => {};
+  //counter를 이 counter 컴포넌트로 가져와야 함
+  const counter = useSelector((state) => state.counter); //3
+  const dispatch = useDispatch(); //4
+
+  const incrementHandler = () => {
+    dispatch({ type: "increment" });
+  };
+  const decrementHandelr = () => {
+    dispatch({ type: "decrement" });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- 카운터 값 --</div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>토글 카운터</button>
+      <button onClick={incrementHandler}>+1</button>
+      <button onClick={decrementHandelr}>-1</button>
     </main>
   );
 };
---
+++ src/store/index.js
@@ -0,0 +1,20 @@
+//1, store - index.js
+import { createStore } from "redux";
+
+const counterReudcer = (state = { counter: 0 }, action) => {
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+  return state;
+};
+
+const store = createStore(counterReudcer);
+
+export default store;
