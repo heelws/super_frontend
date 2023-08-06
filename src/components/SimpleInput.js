ac04524f478bcace6df63b43fe4c8b3abf1684f5
heelws <162127@naver.com>
2023-08-06 15:41:11 +0900

    form 제출 시 유효성 검사
    1. 이름 값을 상태로 저장
    2. Form 제출 및 ref 적용
    3. 이름 값이 없을 때 제출하지 않게 분기 처리
    4. 유효하지 않은 이름 값일 때 에러 메시지 노출
    5. 제출 시점과 터치 시점을 구분해 주기

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,53 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+  const nameInputRef = useRef(); //2
+  const [enteredName, setEnteredName] = useState(" "); //1
+  const [enterendNameIsValid, setEnteredNameIsValid] = useState(false); //4
+  const [enteredNameIsTouched, setenteredNameIsTouched] = useState(false); //5 처음 enteredName이 touch X => false
+
+  //input창의 값을 바꿀 때마다 그 값을 enteredName에 적절하게 변화 시켜주는 역할
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    console.log("enteredName", enteredName);
+
+    setenteredNameIsTouched(true);
+
+    //3 enteredName이 빈 값인 경우 제출 안되게
+    if (enteredName.trim() === "") {
+      console.log("submit fail");
+      setEnteredNameIsValid(false);
+      return;
+    }
+    setEnteredNameIsValid(true); //enteredName이 값을 가지고 있다
+    setEnteredName("");
+  };
+
+  //3 에러 메시지를 보여주는 경우, true -> 메시지 노출
+  const nameInputIsInvalid = !enterendNameIsValid && enteredNameIsTouched;
+
+  const nameInputClasses = nameInputIsInvalid
+    ? "form-control invalid"
+    : "form-control";
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>당신의 이름은?</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmitHandler}>
+      <div className={nameInputClasses}>
+        <label htmlFor="name">당신의 이름은?</label>
+        <input
+          type="text"
+          id="name"
+          ref={nameInputRef}
+          value={enteredName}
+          onChange={nameInputChangeHandler}
+        />
+        {nameInputIsInvalid && (
+          <p className="error-text">이름값은 빈 값이 아니어야 합니다.</p> //4
+        )}
       </div>
       <div className="form-actions">
         <button>제출하기</button>
